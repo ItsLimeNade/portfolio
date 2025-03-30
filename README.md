5cc5a3a7e775ba566e84d34009a07c171dc637a1
LimeNade <alexandre.zarkua@gmail.com>
2025-03-30 16:22:36 +0200

    Update

--- README.md
+++
@@ -1,2 +0,0 @@
-# portfolio
-Just my portfolio
--- portfolio/src/routes/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    import "../app.css";
-</script>
-
-<div class="h-screen bg-gradient-to-r from-[rgb(11,11,37)] to-[rgb(35,11,37)] thingy"> 
-    <slot />
-</div>
